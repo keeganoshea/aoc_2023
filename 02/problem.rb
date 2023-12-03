49db6fbc8aaca4922820218a025938a361862f71
keeganoshea <koshea@doximity.com>
2023-12-02 20:50:07 -0700

    Day 2 Initial Solution

---
+++ 02/problem.rb
@@ -0,0 +1,142 @@
+class CubeConundrum
+  attr_accessor :games,
+                :red_cubes_loaded,
+                :blue_cubes_loaded,
+                :green_cubes_loaded,
+                :impossible_games
+
+  def initialize(games, red_cubes_loaded: 0, green_cubes_loaded: 0, blue_cubes_loaded: 0)
+    @games = games.split("\n").map do |game|
+      Game.new(game)
+    end
+    @red_cubes_loaded = red_cubes_loaded
+    @green_cubes_loaded = green_cubes_loaded
+    @blue_cubes_loaded = blue_cubes_loaded
+  end
+
+  def possible_games_max
+    impossible_games = check_game_possiblity_max.uniq
+    games - impossible_games
+  end
+
+  def sum_possible_game_ids
+    possible_games_max.map(&:id).sum
+  end
+
+  def check_game_possiblity_max
+    impossible_games = []
+    games.each do |game|
+      if game.set.red_cubes_max > red_cubes_loaded
+        impossible_games << game
+      end
+      if game.set.green_cubes_max > green_cubes_loaded
+        impossible_games << game
+      end
+      if game.set.blue_cubes_max > blue_cubes_loaded
+        impossible_games << game
+      end
+    end
+    impossible_games
+  end
+
+  def power_of_sets
+    check_game_possiblity_min.sum
+  end
+
+  def check_game_possiblity_min
+    games.map do |game|
+      [
+        game.set.red_cubes_max,
+        game.set.green_cubes_max,
+        game.set.blue_cubes_max
+      ].inject(:*)
+    end
+  end
+end
+
+class Game
+  attr_accessor :set, :name, :id
+  def initialize(game)
+    @name = game.delete(" \t\r\n").split(":").first
+    @id = name.delete("Game").to_i
+    @set = Set.new(game.delete(" \t\r\n").split(":").last)
+  end
+end
+
+class Set
+  attr_reader :plays
+  def initialize(set)
+    @sets = set.split(";")
+    @plays = @sets.map do |set|
+      set.split(",")
+    end.flatten
+  end
+
+  def red_cubes_max
+    nums = []
+    plays.each do |play|
+      if play.scan(/red/).any?
+        nums << play.scan(/-?\d+/).first.to_i
+      end
+    end
+    nums.max
+  end
+
+  def green_cubes_max
+    nums = []
+    plays.each do |play|
+      if play.scan(/green/).any?
+        nums << play.scan(/-?\d+/).first.to_i
+      end
+    end
+    nums.max
+  end
+
+  def blue_cubes_max
+    nums = []
+    plays.each do |play|
+      if play.scan(/blue/).any?
+        nums << play.scan(/-?\d+/).first.to_i
+      end
+    end
+    nums.max
+  end
+
+  def count_red
+    count = 0
+    plays.each do |play|
+      if play.scan(/red/).any?
+        count += play.scan(/-?\d+/).first.to_i
+      end
+    end
+    count
+  end
+
+  def count_blue
+    count = 0
+    plays.each do |play|
+      if play.scan(/blue/).any?
+        count += play.scan(/-?\d+/).first.to_i
+      end
+    end
+    count
+  end
+
+  def count_green
+    count = 0
+    plays.each do |play|
+      if play.scan(/green/).any?
+        count += play.scan(/-?\d+/).first.to_i
+      end
+    end
+    count
+  end
+end
+
+games = File.read(ARGV[0])
+game = CubeConundrum.new(games, red_cubes_loaded: 12, green_cubes_loaded: 13, blue_cubes_loaded: 14)
+puts "Part 1: #{game.sum_possible_game_ids}"
+# part 2
+puts "Part 2: #{game.power_of_sets}"
+
+
