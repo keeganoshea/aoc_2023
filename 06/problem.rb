1b3bf9751582d6d12dcaca5f7290d7e61b2fedb5
keeganoshea <koshea@doximity.com>
2023-12-06 21:39:26 -0700

    Day 6 Part 2

--- 06/problem.rb
+++ 06/problem.rb
@@ -1,16 +1,26 @@
 class Competition
   attr_reader :races
-  def initialize(data)
-    @races = create_races(data)
+  def initialize(data, multi: true)
+    @races = create_races(data, multi)
   end
 
-  def create_races(data)
-    time = data.split("\n").first.scan(/-?\d+/)
-    distance = data.split("\n").last.scan(/-?\d+/)
-    time_distance = time.zip(distance)
-    time_distance.map do |td|
-      BoatRace.new(td.first.to_i, td.last.to_i)
+  def create_races(data, multi)
+    if multi
+      time = data.split("\n").first.scan(/-?\d+/)
+      distance = data.split("\n").last.scan(/-?\d+/)
+      time_distance = time.zip(distance)
+      time_distance.map do |td|
+        BoatRace.new(td.first.to_i, td.last.to_i)
+      end
+    else
+      time = data.split("\n").first.split(":").last.gsub(" ", "")
+      distance = data.split("\n").last.split(":").last.gsub(" ", "")
+      [BoatRace.new(time.to_i, distance.to_i)]
     end
+  end
+
+  def count_of_race_wins
+    races.first.count_of_winning_races
   end
 
   def product_of_winning_races
@@ -52,5 +62,7 @@ end
 
 
 input = File.read(ARGV[0])
-competition = Competition.new(input)
-puts competition.product_of_winning_races
+competition_1 = Competition.new(input)
+competition_2 = Competition.new(input, multi: false)
+puts "Part 1: #{competition_1.product_of_winning_races}"
+puts "Part 2: #{competition_2.count_of_race_wins}"
